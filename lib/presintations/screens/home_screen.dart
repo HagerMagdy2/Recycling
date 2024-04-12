26b47dcec352940644848b6b931289510562c74e
Arwa Ayman <109555559+arwaayman16@users.noreply.github.com>
2024-04-12 16:18:59 +0200

    Update home_screen.dart

--- lib/presintations/screens/home_screen.dart
+++ lib/presintations/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firstly/constants.dart';
 import 'package:firstly/controller/home-page-controller.dart';
+import 'package:firstly/presintations/screens/add_product.dart';
+import 'package:firstly/presintations/widgets/bottom-bar.dart';
 import 'package:firstly/presintations/widgets/drawer.dart';
 import 'package:firstly/presintations/widgets/item.dart';
 import 'package:firstly/presintations/widgets/matrial_widget.dart';
@@ -15,101 +17,103 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Get.put(HomeControllerImp());
     return GetBuilder<HomeControllerImp>(
-      builder: (controller) => Scaffold(
-        drawer: AppDrawer(),
-        appBar: AppBar(
-          foregroundColor: kMainColor,
-          backgroundColor: Colors.white,
-          title: const Text(
-            'Be Green',
-            style: TextStyle(fontWeight: FontWeight.bold, color: kMainColor),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        body: controller.pagesList.elementAt(controller.currentPage),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-          backgroundColor: kMainColor,
-          elevation: 10,
-        ),
-        bottomNavigationBar: BottomAppBar(
-          height: 50.0,
-          notchMargin: 5.0,
-          shape: CircularNotchedRectangle(),
-          color: Colors.white,
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 0),
-                  child: MaterialButton(
-                    onPressed: () => controller.changePage(0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.home,
-                          color: Colors.grey,
+        builder: (controller) => Scaffold(
+            drawer: AppDrawer(),
+            appBar: AppBar(
+              foregroundColor: kMainColor,
+              backgroundColor: Colors.white,
+              title: const Text(
+                'Be Green',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: kMainColor),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            body: controller.pagesList.elementAt(controller.currentPage),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+              backgroundColor: kMainColor,
+              elevation: 10,
+            ),
+            bottomNavigationBar: BottomAppBar(
+              height: 50.0,
+              notchMargin: 5.0,
+              shape: CircularNotchedRectangle(),
+              color: Colors.white,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 0),
+                      child: MaterialButton(
+                        onPressed: () => controller.changePage(0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.home,
+                              color: Colors.grey,
+                            ),
+                            Text('Home', style: TextStyle(color: Colors.grey))
+                          ],
                         ),
-                        Text('Home', style: TextStyle(color: Colors.grey))
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: MaterialButton(
-                    onPressed: () => controller.changePage(1),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.favorite,
-                          color: Colors.grey,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: MaterialButton(
+                        onPressed: () => controller.changePage(1),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.favorite,
+                              color: Colors.grey,
+                            ),
+                            Text('Favorite',
+                                style: TextStyle(color: Colors.grey))
+                          ],
                         ),
-                        Text('Favorite', style: TextStyle(color: Colors.grey))
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: MaterialButton(
-                    onPressed: () => controller.changePage(2),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.lightbulb,
-                          color: Colors.grey,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: MaterialButton(
+                        onPressed: () => controller.changePage(2),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.lightbulb,
+                              color: Colors.grey,
+                            ),
+                            Text('Learn', style: TextStyle(color: Colors.grey))
+                          ],
                         ),
-                        Text('Learn', style: TextStyle(color: Colors.grey))
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: MaterialButton(
-                    onPressed: () => controller.changePage(3),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.person,
-                          color: Colors.grey,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: MaterialButton(
+                        onPressed: () => controller.changePage(3),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.person,
+                              color: Colors.grey,
+                            ),
+                            Text('Profile',
+                                style: TextStyle(color: Colors.grey))
+                          ],
                         ),
-                        Text('Profile', style: TextStyle(color: Colors.grey))
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-              ]),
-        ),
-      ),
-    );
+                  ]),
+            )));
   }
 }

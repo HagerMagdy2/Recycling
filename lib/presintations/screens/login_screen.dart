af719a8ae9688ec4c1c1621e4c4a9a3c0894714e
Arwa Ayman <109555559+arwaayman16@users.noreply.github.com>
2024-04-12 16:13:53 +0200

    Update login_screen.dart

--- lib/presintations/screens/login_screen.dart
+++ lib/presintations/screens/login_screen.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:firstly/constants.dart';
 import 'package:firstly/presintations/bloc/authentication_bloc.dart';
 import 'package:firstly/presintations/provider/adminMode.dart';
 import 'package:firstly/presintations/screens/signup_screen.dart';
 import 'package:firstly/presintations/screens/home_screen.dart';
 import 'package:firstly/presintations/widgets/custom_textfield.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
@@ -12,7 +13,7 @@ import 'package:provider/provider.dart';
 class LoginScreen extends StatefulWidget {
   static String id = 'LoginScreen';
 
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -23,6 +24,7 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController EmailC = TextEditingController();
   TextEditingController PasswordC = TextEditingController();
   bool isAdmin = false;
+  bool isPasswordIncorrect = false;
 
   @override
   void initState() {
@@ -34,200 +36,235 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: BlocConsumer<AuthenticationBloc, AuthenticationState>(
-        listener: (context, state) {
-          if (state is Authorized) {
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (_) => const HomeScreen()));
-          }
-        },
-        builder: (context, state) {
-          return Form(
-            key: _globalKey,
-            child: ListView(
-              children: [
-                if (state is AuthLoding)
-                  Lottie.asset(
-                    'assets/images/Animation loading1.json',
-                    height: 100,
-                    width: 100,
-                    repeat: true,
-                  ),
-                if (state is AuthError) const Text('Error'),
-                Padding(
-                  padding: const EdgeInsets.only(top: 45),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.32,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 70),
-                          child: Container(
-                            // color: Colors.amber,
-                            height: 200,
-                            width: 300,
-                            child: Image.asset(
-                              "assets/images/Mobile login2.png",
-                              fit: BoxFit.cover,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: BlocConsumer<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            if (state is Authorized) {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (_) => const HomeScreen()),
+              );
+            }
+          },
+          builder: (context, state) {
+            return Form(
+              key: _globalKey,
+              child: ListView(
+                children: [
+                  if (state is AuthLoding)
+                    Lottie.asset(
+                      'assets/images/Animation loading1.json',
+                      height: 200,
+                      width: 200,
+                      repeat: true,
+                    ),
+                  if (state is AuthError) const Text('Error'),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 45),
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.32,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 70),
+                            child: Container(
+                              height: 200,
+                              width: 300,
+                              child: Image.asset(
+                                "assets/images/Mobile login2.png",
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 155),
-                          child: Text(
-                            'Be',
-                            style: TextStyle(
+                          Padding(
+                            padding: EdgeInsets.only(top: 155),
+                            child: Text(
+                              'Be',
+                              style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 50,
-                                fontWeight: FontWeight.bold),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 210),
-                          child: Text(
-                            'Green',
-                            style: TextStyle(
+                          Padding(
+                            padding: EdgeInsets.only(top: 210),
+                            child: Text(
+                              'Green',
+                              style: TextStyle(
                                 fontSize: 50,
                                 color: kMainColor,
-                                fontWeight: FontWeight.w900),
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
                           ),
-                        )
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: height * 0.1,
-                ),
-                CustomTextField(
-                  controller: EmailC,
-                  hint: 'Enter your email',
-                  icon: Icons.email,
-                ),
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                CustomTextField(
-                  controller: PasswordC,
-                  hint: 'Enter your password',
-                  icon: Icons.lock,
-                ),
-                SizedBox(
-                  height: height * 0.05,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 120),
-                  child: TextButton(
+                  SizedBox(
+                    height: height * 0.1,
+                  ),
+                  CustomTextField(
+                    controller: EmailC,
+                    hint: 'Enter your email',
+                    icon: Icons.email,
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  CustomTextField(
+                    controller: PasswordC,
+                    hint: 'Enter your password',
+                    icon: Icons.lock,
+                  ),
+                  Visibility(
+                    visible: isPasswordIncorrect,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.error_outline,
+                              color: Colors.red,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              'Incorrect password',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 12.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * 0.05,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 120),
+                    child: TextButton(
                       style: TextButton.styleFrom(
                         backgroundColor: kMainColor,
-                        shape: const RoundedRectangleBorder(
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.all(Radius.circular(20)),
                         ),
                       ),
-                      onPressed: () {
+                      onPressed: () async {
                         if (_globalKey.currentState!.validate()) {
-                          context.read<AuthenticationBloc>().add(SignInEvent(
-                                email: EmailC.text,
-                                password: PasswordC.text,
-                              ));
-                          if (state is UnAuthorized)
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15.0),
-                                ),
-                                title: const Text('Sign Up Required'),
-                                content: const Text(
-                                  'Please sign up first before logging in.',
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text(
-                                      'OK',
-                                      style: TextStyle(color: kMainColor),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
+                          bool isPasswordCorrect = await _checkPassword(
+                            EmailC.text,
+                            PasswordC.text,
+                          );
+
+                          if (isPasswordCorrect) {
+                            context.read<AuthenticationBloc>().add(SignInEvent(
+                                  email: EmailC.text,
+                                  password: PasswordC.text,
+                                ));
+                          } else {
+                            setState(() {
+                              isPasswordIncorrect = true;
+                            });
+                          }
                         }
                       },
                       child: const Text(
                         'Login',
                         style: TextStyle(color: Colors.white),
-                      )),
-                ),
-                SizedBox(
-                  height: height * 0.05,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Don\'t have an account ? ',
-                      style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
                     ),
-                    GestureDetector(
+                  ),
+                  SizedBox(
+                    height: height * 0.05,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Don\'t have an account ? ',
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                      GestureDetector(
                         onTap: () {
                           Navigator.pushNamed(context, SignupScreen.id);
                         },
                         child: const Text(
                           'Signup',
                           style: TextStyle(
-                              fontSize: 16,
-                              color: kMainColor,
-                              fontWeight: FontWeight.bold),
-                        ))
-                  ],
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          child: GestureDetector(
-                        onTap: () {
-                          Provider.of<AdminMode>(context, listen: false)
-                              .changeIsAdmin(false);
-                        },
-                        child: Text(
-                          'I\'m an admin',
-                          style: TextStyle(
-                            color: Provider.of<AdminMode>(context).isAdmin
-                                ? kMainColor
-                                : Colors.white,
+                            fontSize: 16,
+                            color: kMainColor,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      )),
-                      Expanded(
-                          child: GestureDetector(
-                        onTap: () {
-                          Provider.of<AdminMode>(context, listen: false)
-                              .changeIsAdmin(true);
-                        },
-                        child: Text(
-                          'I\'m a user',
-                          style: TextStyle(
-                            color: Provider.of<AdminMode>(context).isAdmin
-                                ? Colors.white
-                                : kMainColor,
-                          ),
-                        ),
-                      )),
+                      ),
                     ],
                   ),
-                )
-              ],
-            ),
-          );
-        },
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Provider.of<AdminMode>(context, listen: false)
+                                  .changeIsAdmin(false);
+                            },
+                            child: Text(
+                              'I\'m an admin',
+                              style: TextStyle(
+                                color: Provider.of<AdminMode>(context).isAdmin
+                                    ? kMainColor
+                                    : Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () {
+                              Provider.of<AdminMode>(context, listen: false)
+                                  .changeIsAdmin(true);
+                            },
+                            child: Text(
+                              'I\'m a user',
+                              style: TextStyle(
+                                color: Provider.of<AdminMode>(context).isAdmin
+                                    ? Colors.white
+                                    : kMainColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
       ),
-    ));
+    );
+  }
+
+  Future<bool> _checkPassword(String email, String password) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return true;
+    } on FirebaseAuthException catch (e) {
+      print('Error: $e');
+      return false;
+    }
   }
 }

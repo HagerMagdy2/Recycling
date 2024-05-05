8089385e6d09cf6acf72e4f3a0f39f05ac6d7e1e
Hager Magdy <120759962+HagerMagdy2@users.noreply.github.com>
2024-05-05 15:47:15 +0300

    Update favorite_screen.dart

--- lib/presintations/screens/home/favorite_screen.dart
+++ lib/presintations/screens/home/favorite_screen.dart
@@ -1,14 +1,97 @@
-
+import 'package:firstly/constants.dart';
+import 'package:firstly/presintations/bloc/products_bloc.dart';
+import 'package:firstly/presintations/bloc/products_event.dart';
+import 'package:firstly/presintations/bloc/products_state.dart';
+import 'package:firstly/presintations/screens/home/check_out_page.dart';
+import 'package:firstly/presintations/widgets/show_product.dart';
+import 'package:firstly/presintations/widgets/show_in_cart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+import 'package:firstly/constants.dart';
+import 'package:firstly/data/models/product.dart';
+import 'package:firstly/presintations/bloc/products_bloc.dart';
+import 'package:firstly/presintations/bloc/products_event.dart';
+import 'package:firstly/presintations/bloc/products_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
 
 
-class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({super.key});
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<ProductBloc>().add(GetCartProduct());
+  }
+
+  List<Product> getCartProducts() {
+    if (context.read<ProductBloc>().state is ProductLoaded) {
+      return (context.read<ProductBloc>().state as ProductLoaded).products;
+    }
+    return [];
+  }
+
+  double calculateTotalPrice(List<Product> products) {
+    double totalPrice = 0.0;
+    for (var product in products) {
+      totalPrice += product.price * product.quantity;
+    }
+    return totalPrice;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent,
+      backgroundColor: Colors.white,
+      body: BlocBuilder<ProductBloc, ProductState>(
+        builder: (context, state) {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  if (state is ProductLoadingState)
+                    Lottie.asset(
+                      'assets/images/Animation loading1.json',
+                      height: 200,
+                      width: 200,
+                      repeat: true,
+                    ),
+                  if (state is ProductErrorState)
+                    Text('Error: ${state.errorMessage}'),
+                  if (state is ProductLoaded)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: state.products.length,
+                            itemBuilder: (context, i) =>
+                                ShowInCart(product: state.products[i]),
+                          ),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                    )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }

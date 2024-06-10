bb635cec18d412d05b7ab3784a2d414926cd0048
Aya Mohammed <120148097+AyaaMo7ammed@users.noreply.github.com>
2024-06-11 01:31:11 +0300

    Update ProfilePage.dart

--- lib/presintations/screens/ProfilePage.dart
+++ lib/presintations/screens/ProfilePage.dart
@@ -1,183 +1,241 @@
-import 'dart:async';
-
-import 'package:firstly/presintations/User/user.dart';
-// import 'package:firstly/presintations/screens/edit_birthday.dart';
 import 'package:flutter/material.dart';
+import 'package:firstly/constants.dart';
+import 'package:firstly/core/firebase-service.dart';
+import 'package:firstly/core/storage_helper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firstly/presintations/screens/add-edit/edit-profile-screen.dart';
+import 'package:firstly/presintations/widgets/add_profile-photo.dart';
 
-import '../../constants.dart';
-import '../User/user_data.dart';
-import '../widgets/display_image_widget.dart';
-import 'edit_description.dart';
-import 'edit_email.dart';
-import 'edit_image.dart';
-import 'edit_name.dart';
-import 'edit_phone.dart';
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+  static String id = 'ProfileScreen';
 
-class ProfilePage extends StatefulWidget {
-  static String id = 'ProfilePage';
   @override
-  _ProfilePageState createState() => _ProfilePageState();
+  State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
-  var user = UserData.myUser;
-  @override
-  Widget build(BuildContext context) {
-    //final user = UserData.myUser;
+class _ProfileScreenState extends State<ProfileScreen> {
+  late String userId;
+  String? profilePhotoUrl;
+  String? userName;
+  String? userEmail;
+  String? userPhone;
+  bool _isSavingPhoneNumber = false;
 
-    return Scaffold(
-      body: Column(
-        children: [
-          AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            toolbarHeight: 10,
-          ),
-          Center(
-              child: Padding(
-                  padding: EdgeInsets.only(bottom: 20),
-                  child: Text(
-                    'My Profile',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w700,
-                      color: kMainColor1,
-                    ),
-                  ))),
-          InkWell(
-              onTap: () {
-                navigateSecondPage(EditImagePage());
-              },
-              child: DisplayImage(
-                imagePath: user.image,
-                onPressed: () {},
-              )),
-          buildUserInfoDisplay(user.name, 'Name', EditNameFormPage()),
-          buildUserInfoDisplay(user.phone, 'Phone', EditPhoneFormPage()),
-          buildUserInfoDisplay(user.email, 'Email', EditEmailFormPage()),
-          Expanded(
-            child: buildAbout(user),
-            flex: 4,
-          )
-        ],
-      ),
-    );
+  @override
+  void initState() {
+    super.initState();
+    userId = StorageHelperImpl().getCurrentUserId();
+    loadProfileData();
+    loadProfilePhotoUrl();
+    loadPhoneNumber(); // Call loadPhoneNumber here
+    StorageHelperImpl().profilePhotoStream.listen((String newUrl) {
+      setState(() {
+        profilePhotoUrl = newUrl;
+      });
+    });
   }
 
-  // Widget builds the display item with the proper formatting to display the user's info
-  Widget buildUserInfoDisplay(String getValue, String title, Widget editPage) =>
-      Padding(
-          padding: EdgeInsets.only(bottom: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.grey,
-                ),
-              ),
-              SizedBox(
-                height: 1,
-              ),
-              Container(
-                  width: 350,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(
-                    color: Colors.grey,
-                    width: 1,
-                  ))),
-                  child: Row(children: [
-                    Expanded(
-                        child: TextButton(
-                            onPressed: () {
-                              navigateSecondPage(editPage);
-                            },
-                            child: Text(
-                              getValue,
-                              style: TextStyle(fontSize: 16, height: 1.4),
-                            ))),
-                    IconButton(
-                      onPressed: () {
-                        navigateSecondPage(editPage);
-                      },
-                      icon: Icon(
-                        Icons.edit,
-                        color: Colors.grey,
-                      ),
-                      iconSize: 20,
-                    )
-                  ]))
-            ],
-          ));
-
-  // Widget builds the About Me Section
-  Widget buildAbout(User user) => Padding(
-      padding: EdgeInsets.only(bottom: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Tell Us About Yourself',
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
-              color: Colors.grey,
-            ),
-          ),
-          const SizedBox(height: 1),
-          Container(
-              width: 350,
-              height: 100,
-              decoration: BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(
-                color: Colors.grey,
-                width: 1,
-              ))),
-              child: Row(children: [
-                Expanded(
-                    child: TextButton(
-                        onPressed: () {
-                          navigateSecondPage(EditDescriptionFormPage());
-                        },
-                        child: Padding(
-                            padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
-                            child: Align(
-                                alignment: Alignment.topLeft,
-                                child: Text(
-                                  user.aboutMeDescription,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    height: 1.4,
-                                  ),
-                                ))))),
-                IconButton(
-                  onPressed: () {
-                    navigateSecondPage(EditDescriptionFormPage());
-                  },
-                  icon: Icon(
-                    Icons.edit,
-                    color: Colors.grey,
-                  ),
-                  iconSize: 20,
-                )
-              ]))
-        ],
-      ));
-
-  // Refrshes the Page after updating user info.
-  FutureOr onGoBack(dynamic value) {
+  Future<void> loadProfilePhotoUrl() async {
+    profilePhotoUrl = await StorageHelperImpl().getProfilePhotoUrl(userId);
     setState(() {});
   }
 
-  // Handles navigation and prompts refresh.
-  void navigateSecondPage(Widget editForm) {
-    Route route = MaterialPageRoute(builder: (context) => editForm);
-    Navigator.push(context, route).then(onGoBack);
+  Future<void> loadProfileData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        userName = user.displayName;
+        userEmail = user.email;
+      });
+      // Load phone number after loading profile data
+      await loadPhoneNumber();
+    }
+  }
+
+  Future<void> loadPhoneNumber() async {
+    userPhone = await FirebaseService.getUserPhoneNumber(userId);
+    setState(() {});
+  }
+
+  Future<void> updateProfileData(
+      String newName, String newEmail, String newPhone) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      try {
+        await user.updateDisplayName(newName);
+        await user.updateEmail(newEmail);
+        await FirebaseService.saveUserPhoneNumber(userId, newPhone);
+        setState(() {
+          userName = newName;
+          userEmail = newEmail;
+          userPhone = newPhone; // Update local state with the new phone number
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Profile updated successfully')),
+        );
+      } catch (e) {
+        print("Failed to update profile: $e");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to update profile: $e')),
+        );
+      }
+    }
+  }
+
+  Future<void> savePhoneNumber(String phoneNumber) async {
+    setState(() {
+      _isSavingPhoneNumber = true;
+    });
+    try {
+      String? verificationId =
+          await FirebaseService.verifyPhoneNumber(phoneNumber);
+      if (verificationId != null) {
+        await FirebaseService.saveUserPhoneNumber(phoneNumber, verificationId);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Phone number saved successfully')),
+        );
+        print('Current user phone number: $userPhone');
+
+        await loadPhoneNumber(); // Reload phone number after saving
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to send verification code')),
+        );
+      }
+    } catch (e) {
+      print("Failed to verify phone number: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to verify phone number: $e')),
+      );
+    } finally {
+      setState(() {
+        _isSavingPhoneNumber = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () async {
+                await showDialog(
+                  context: context,
+                  builder: (context) => AddProfilePhoto(),
+                );
+                await loadProfileData();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Profile photo set successfully')),
+                );
+              },
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: kMainColor,
+                      radius: 80,
+                      backgroundImage: profilePhotoUrl != null
+                          ? NetworkImage(profilePhotoUrl!)
+                          : null,
+                      child: profilePhotoUrl == null
+                          ? Icon(
+                              Icons.person,
+                              size: 80,
+                              color: Colors.white,
+                            )
+                          : null,
+                    ),
+                    MaterialButton(
+                      onPressed: () async {
+                        await showDialog(
+                          context: context,
+                          builder: (context) => AddProfilePhoto(),
+                        );
+                        await loadProfileData();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text('Profile photo set successfully')),
+                        );
+                      },
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              userName ?? 'User Name',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              userEmail ?? 'User Email',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              userPhone ?? 'User Phone Number',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EditProfileScreen(
+                      currentName: userName ?? '',
+                      currentEmail: userEmail ?? '',
+                      currentPhone: userPhone ?? '',
+                    ),
+                  ),
+                );
+
+                if (result != null && result is Map<String, String>) {
+                  await updateProfileData(
+                    result['name']!,
+                    result['email']!,
+                    result['phone']!,
+                  );
+                }
+              },
+              child: Text('Edit Profile'),
+              style: ElevatedButton.styleFrom(
+                //primary: kMainColor,
+                backgroundColor: kMainColor,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
